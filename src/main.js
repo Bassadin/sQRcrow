0e8378489d78ff154fc6f3e43c2d4f345f702bf8
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-06 23:41:39 +0100

    Setup vuetify? Finally?

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import "@/firebase/";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
