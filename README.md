a163f10580e4f322890b06d981eaa3abc412cc99
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-06 16:35:02 +0100

    Set up vue project

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
-# gamma
-Team Gamma GIS Walter-Weyland
+# sqrcrow
 
-# Theme
-* Location-Based "Social Network"
-* Place QR Codes in the real world and scan them with our app to post comments about the place the QR code is located in
+## Project setup
+```
+npm install
+```
 
-# Technology
-* Vue.js and MongoDB
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,32 @@
 <template>
   <div id="app">
-    <main>
-      <header>
-        <h1>Grasshoppers Finest THC products
-          <img src="./assets/cannabis.png" width="160"/>
-        </h1>
-      </header>
-      <article>
-        <section>
-          <p>Welcome to our cannabis online shop.</p>
-          Please select your country:<br/>
-          <select>
-            <option value="de">Germany</option>
-            <option value="nl">Netherlands</option>
-            <option value="ch">Switzerland</option>
-          </select>
-        </section>
-      </article>
-    </main>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-
-export default {
-  name: 'app',
-  components: {
-  }
-}
-</script>
-
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
