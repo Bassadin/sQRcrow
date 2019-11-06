13ead5364d65fa91f7178117f878aa6f9f0d5395
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-06 17:13:51 +0100

    Set up firebase stuff

---
+++ src/firebase/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC2fbvAz0oImUUom33JizMTFuejFadbj2A",
+    authDomain: "sqrcrow-7c7f8.firebaseapp.com",
+    databaseURL: "https://sqrcrow-7c7f8.firebaseio.com",
+    projectId: "sqrcrow-7c7f8",
+    storageBucket: "sqrcrow-7c7f8.appspot.com",
+    messagingSenderId: "603754005895",
+    appId: "1:603754005895:web:62176752a3f1f936ebff22",
+    measurementId: "G-GFQCFHGEDY"
+  };
+firebase.initializeApp(config);
