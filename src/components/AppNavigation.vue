6ccc08f4c5137bf2e13f59e4ac635eda76ee2698
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-07 15:02:40 +0100

    More page updates

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,13 +1,12 @@
 <template>
   <v-app-bar app color="brown darken-4" dark>
     <v-spacer class="hidden-md-and-up"></v-spacer>
-    <v-toolbar-title>blubb</v-toolbar-title>
-    <v-btn to="/" text class="hidden-sm-and-down">Home</v-btn>
+    <v-toolbar-title>sQRcrow</v-toolbar-title>
+    <v-btn to="/">Home</v-btn>
+    <v-btn to="/about">About</v-btn>
     <v-spacer></v-spacer>
-    <v-btn to="/about" text class="hidden-sm-and-down">About</v-btn>
-    <v-spacer class="hidden-sm-and-down"></v-spacer>
-    <v-btn text class="hidden-sm-and-down">SIGN IN</v-btn>
-    <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
+    <v-btn text>SIGN IN</v-btn>
+    <v-btn color="brown lighten-3">JOIN</v-btn>
   </v-app-bar>
 </template>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>This is Team Gamma and this is our favorite page on the internet.</p>
   </div>
 </template>
