64b5f0f5879be3203d99189a1b1c6c23a355da1b
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-07 15:31:19 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <p>This is Team Gamma and this is our favorite page on the internet.</p>
+    <v-container text-center wrap>
+      <v-flex xs12>
+        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img>
+      </v-flex>
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">This is an about page</h1>
+        <p
+          class="subheading font-weight-regular"
+        >This is Team Gamma and this is our favorite page on the internet.</p>
+      </v-flex>
+    </v-container>
   </div>
 </template>
