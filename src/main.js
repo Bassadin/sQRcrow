32632bc3022be1f63d34306e469f2bfffea6d495
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-10 12:22:22 +0100

    Maps stuff (API key missing)

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VueQrcodeReader from "vue-qrcode-reader";
+import * as VueGoogleMaps from "vue2-google-maps";
+
+//Import Google Maps stuff
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", //TODO: Get API key
+        libraries: "places,drawing,visualization"
+    }
+});
 
 Vue.use(VueQrcodeReader);
 Vue.config.productionTip = false;
