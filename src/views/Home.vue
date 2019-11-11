1afac69b7beb0f0c308d44fcd6443039d06531ec
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-11 18:21:53 +0100

    Merged helloworld vue into home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
     <div class="home">
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <v-layout text-center wrap>
+            <v-flex xs12>
+                <v-img
+                    :src="require('../assets/sQRcrow_logo.svg')"
+                    class="my-3"
+                    contain
+                    height="200"
+                ></v-img>
+            </v-flex>
+
+            <v-flex mb-4>
+                <h1 class="display-2 font-weight-bold mb-3">
+                    Welcome to sQRcrow
+                </h1>
+                <p class="subheading font-weight-regular">
+                    Click on <a href="/about">about</a> at the top to try out
+                    the qr code scanner
+                </p>
+            </v-flex>
+        </v-layout>
     </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-    name: 'home',
-    components: {
-        HelloWorld
-    }
-};
-</script>
+<script></script>
