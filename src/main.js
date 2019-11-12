c57c7ba2a52c5caf79ec202a87b49a0a3fa2371c
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-12 14:24:07 +0100

    Removed Google Maps

--- src/main.js
+++ src/main.js
@@ -4,20 +4,11 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import VueQrcodeReader from 'vue-qrcode-reader';
-import * as VueGoogleMaps from 'vue2-google-maps';
 import VueAnalytics from 'vue-analytics';
 
 Vue.use(VueAnalytics, {
     id: process.env.VUE_APP_GOOGLE_ANALYTICS_TRACKING_CODE,
     checkDuplicatedScript: true
-});
-
-//Import Google Maps stuff
-Vue.use(VueGoogleMaps, {
-    load: {
-        key: process.env.VUE_APP_MAPS_API_KEY,
-        libraries: 'places,drawing,visualization'
-    }
 });
 
 Vue.use(VueQrcodeReader);
