c2fba34fce6aef2b58e94900694d28cd2f55d433
Tawan <nansintawan99@gmail.com>
2019-11-25 16:17:45 +0100

    Add cards to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,10 @@
         <v-layout text-center wrap>
             <v-flex xs12>
                 <v-img
-                    :src="require('../assets/logos/sQRcrow_logo.svg')"
-                    class="my-3"
-                    contain
-                    height="200"
+                        :src="require('../assets/logos/sQRcrow_logo.svg')"
+                        class="my-3"
+                        contain
+                        height="200"
                 ></v-img>
             </v-flex>
 
@@ -20,23 +20,54 @@
                 </p>
             </v-flex>
         </v-layout>
-        <v-card max-height="420" max-width="250">
-            <v-card-text>
-
-            </v-card-text>
-        </v-card>
+        <v-container fluid>
+            <v-row dense>
+                <v-col  max-height="auto" max-width="auto"
+                        v-for="item in newsContent.news"
+                        :key="item"
+                        :cols="item.flex">
+                    <v-card>
+                        <v-img
+                            src="http://picsum.photos/700/500"
+                            gradient="to bottom, rgba(0.1,0.5,0.2,0.01),rgba(0.1,0.5,0.2,0.01)"
+                            height="200px"
+                        >
+                            <h2>{{item.title}}</h2>
+                        </v-img>
+                        <v-card-text>
+                            <p>
+                                {{item.content}}
+                            </p>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
 <script>
-    var newsData = require("../database/news");
 
     export default {
-        data(){
-            return{
-                newsContent: newsData.news
-            }
+        data() {
+            return {
+                newsContent: {}
+            };
 
+        },
+        created() {
+            this.newsContent = require('../database/news');
         }
-        }
+    };
 </script>
+
+<style scoped>
+    .v-card {
+        color: #000000;
+        font-size: 22px;
+    }
+    h2{
+        color: #cccccc;
+        font-size: 30px;
+    }
+</style>
