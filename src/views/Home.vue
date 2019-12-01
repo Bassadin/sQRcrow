ea253799abe7eed94627697ec1b49848e59e7227
KarenJosten <karen.josten@hs-furtwangen.de>
2019-12-01 12:19:14 +0100

    Add comments to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,110 @@
 <template>
-    <div class="home">
-        <v-layout text-center wrap>
-            <v-flex xs12>
-                <v-img
-                    :src="require('../assets/logos/sQRcrow_logo.svg')"
-                    class="my-3"
-                    contain
-                    height="200"
-                ></v-img>
-            </v-flex>
 
-            <v-flex mb-4>
-                <h1 class="display-2 font-weight-bold mb-3">
-                    Welcome to sQRcrow
-                </h1>
-                <p class="subheading font-weight-regular">
-                    Click on <a href="/about">about</a> at the top to try out
-                    the qr code scanner
-                </p>
-            </v-flex>
-        </v-layout>
-    </div>
+    <v-row justify="center">
+        <v-col cols="12" sm="8" md="6">
+            <v-card>
+                <v-toolbar color="navy" dark>
+
+                    <v-toolbar-title>Kommentare</v-toolbar-title>
+
+                </v-toolbar>
+
+                <v-list two-line>
+                    <template v-for="(item, index) in items">
+                        <v-subheader
+                                v-if="item.header"
+                                :key="item.header"
+                                inset
+                        >
+                            {{ item.header }}
+                        </v-subheader>
+
+                        <v-divider
+                                v-else-if="item.divider"
+                                :key="index"
+                                inset
+                        ></v-divider>
+
+                        <v-list-item
+                                v-else
+                                :key="item.title"
+                                ripple
+
+                        >
+                            <v-list-item-avatar>
+                                <img :src="item.avatar">
+                            </v-list-item-avatar>
+                            <v-list-item-content>
+                                <v-list-item-title v-html="item.title"></v-list-item-title>
+                                <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </template>
+                </v-list>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 
-<script></script>
+<script>
+
+    export default {
+        data () {
+            return {
+                items: [
+                    {
+                        header: 'Aktuell',
+                    },
+                    { divider: true },
+                    {
+                        avatar: 'https://picsum.photos/250/300?image=660',
+                        title: 'Neues aus Furtwangen',
+                        subtitle:
+                            "<span class='text--primary'>Julian</span> &mdash; Hallo",
+                    },
+                    {
+                        avatar: 'https://picsum.photos/250/300?image=821',
+                        title: 'Ersti Party <span class="grey--text text--lighten-1"></span>',
+                        subtitle:
+                            "<span class='text--primary'>Lea</span> &mdash; Ich wünschte ich könnte kommen.",
+                    },
+                    {
+                        avatar: 'https://picsum.photos/250/300?image=783',
+                        title: ':)',
+                        subtitle:
+                            "<span class='text--primary'>Sabrina</span> &mdash; hallo zusammen",
+                    },
+                    {
+                        header: 'Gestern',
+                    },
+                    { divider: true },
+                    {
+                        avatar: 'https://picsum.photos/250/300?image=1006',
+                        title: 'Haustiere?',
+                        subtitle:
+                            "<span class='text--primary'>Mia</span> &mdash; Meine neue Katze, süß oder?",
+                    },
+
+                    {
+                        header: 'Letzte Woche',
+                    },
+                    { divider: true },
+                    {
+                        avatar: 'https://picsum.photos/250/300?image=1008',
+                        title: 'Mensa',
+                        subtitle:
+                            "<span class='text--primary'>Nick</span> &mdash; Das gab es heute in der Mensa",
+                    },
+
+
+                ],
+            }
+        },
+    }
+
+</script>
+
+<style>
+
+
+</style>
