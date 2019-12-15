dab29b07908e879ab33d909523f7ab6a8ba72536
Bastian Hodapp <bastianhodapp@gmail.com>
2019-12-16 00:00:19 +0100

    Linted some stuff

--- src/views/Join.vue
+++ src/views/Join.vue
@@ -2,11 +2,8 @@
     <div class="Join">
         <v-content>
             <v-container class="fill-height" fluid>
-
                 <v-row align="center" justify="center">
-
                     <v-col cols="12" sm="8" md="4">
-
                         <v-flex mb-4>
                             <div class="h1 headline text-center">
                                 <h1>Join</h1>
@@ -14,80 +11,101 @@
                         </v-flex>
 
                         <v-card class="elevation-12">
-                            <v-toolbar color="#546e7a" light >
+                            <v-toolbar color="#546e7a" light>
                                 <div class="welcome">
-                                    <v-toolbar-title>Registriere dich bei sQRcrow</v-toolbar-title>
+                                    <v-toolbar-title
+                                        >Registriere dich bei
+                                        sQRcrow</v-toolbar-title
+                                    >
                                 </div>
                                 <v-spacer />
-
                             </v-toolbar>
                             <v-card-text>
-                                <v-form ref="form"
-                                        v-model="valid">
+                                <v-form ref="form" v-model="valid">
                                     <v-text-field
-                                            v-model="forename"
-                                            label="Vorname"
-                                            :rules="nameRules"
-                                            name="forename"
-                                            prepend-icon="mdi-account-box"
-                                            type="forename"
+                                        v-model="forename"
+                                        label="Vorname"
+                                        :rules="nameRules"
+                                        name="forename"
+                                        prepend-icon="mdi-account-box"
+                                        type="forename"
                                     />
                                     <v-text-field
-                                            v-model="familyname"
-                                            label="Nachname"
-                                            :rules="nameRules"
-                                            name="familyname"
-                                            prepend-icon="mdi-account-box"
-                                            type="familyname"
+                                        v-model="familyname"
+                                        label="Nachname"
+                                        :rules="nameRules"
+                                        name="familyname"
+                                        prepend-icon="mdi-account-box"
+                                        type="familyname"
                                     />
                                     <v-text-field
-                                            label="E-Mail"
-                                            :rules="emailRules"
-                                            name="email"
-                                            prepend-icon="mdi-email"
-                                            type="email"
+                                        label="E-Mail"
+                                        :rules="emailRules"
+                                        name="email"
+                                        prepend-icon="mdi-email"
+                                        type="email"
                                     />
                                     <v-text-field
-                                            v-model="username"
-                                            :rules="nameRules"
-                                            label="Benutzername"
-                                            name="username"
-                                            prepend-icon="mdi-account-circle"
-                                            type="username"
+                                        v-model="username"
+                                        :rules="nameRules"
+                                        label="Benutzername"
+                                        name="username"
+                                        prepend-icon="mdi-account-circle"
+                                        type="username"
                                     />
 
                                     <v-text-field
-                                            label="Passwort"
-                                            name="password"
-                                            :rules="passwordRules"
-                                            prepend-icon="mdi-lock"
-                                            :append-icon="value ? 'mdi-eye-off' : 'mdi-eye'"
-                                            @click:append="() => (value = !value)"
-                                            :type="value ? 'password' : 'text'"
-                                            :counter="10"
+                                        label="Passwort"
+                                        name="password"
+                                        :rules="passwordRules"
+                                        prepend-icon="mdi-lock"
+                                        :append-icon="
+                                            value ? 'mdi-eye-off' : 'mdi-eye'
+                                        "
+                                        @click:append="() => (value = !value)"
+                                        :type="value ? 'password' : 'text'"
+                                        :counter="10"
                                     />
                                 </v-form>
                             </v-card-text>
 
                             <div class="check">
-                                <input type="checkbox" id="checkbox" v-model="checked" :rules="[v => !!v || 'Du musst zustimmen, um fortzufahren!']">
-                                <label for="checkbox">{{ label=" ich habe die AGB gelesen und akzeptiere diese" }}</label>
+                                <input
+                                    type="checkbox"
+                                    id="checkbox"
+                                    v-model="checked"
+                                    :rules="[
+                                        v =>
+                                            !!v ||
+                                            'Du musst zustimmen, um fortzufahren!'
+                                    ]"
+                                />
+                                <label for="checkbox">{{
+                                    (label =
+                                        ' ich habe die AGB gelesen und akzeptiere diese')
+                                }}</label>
                             </div>
 
                             <v-card-actions>
                                 <v-spacer />
                                 <div class="btn">
-                                    <v-btn color="#546e7a" icon="" @click="validate">registrieren</v-btn>
+                                    <v-btn
+                                        color="#546e7a"
+                                        icon=""
+                                        @click="validate"
+                                        >registrieren</v-btn
+                                    >
                                 </div>
                             </v-card-actions>
                         </v-card>
 
                         <div class="next text-center">
-                            <p>Du hast schon einen Account? Melde dich <a href="/Login">hier</a> an</p>
+                            <p>
+                                Du hast schon einen Account? Melde dich
+                                <a href="/Login">hier</a> an
+                            </p>
                         </div>
-
                     </v-col>
-
                 </v-row>
             </v-container>
         </v-content>
@@ -99,13 +117,11 @@
                 </v-col>
             </v-footer>
         </div>
-
     </div>
 </template>
 
 <script>
-
-  //  import db from '../db'
+//  import db from '../db'
 
 export default {
     name: 'Join',
@@ -119,74 +135,74 @@ export default {
             username: '',
 
             nameRules: [
-                v => !!v || 'Name is requiered',
+                v => !!v || 'Name is requiered'
                 //v => (v && v.length <= 10 || 'Name must be less than 10 characters')
             ],
             emailRules: [
                 v => !!v || 'E-Mail is requiered',
-                v => (v && v.length <= 10 || 'Name must be less than 10 characters')
+                v =>
+                    (v && v.length <= 10) ||
+                    'Name must be less than 10 characters'
             ],
 
             passwordRules: [
                 v => !!v || 'Password is requiered',
-                v => (v && v.length <= 10 || 'Password must be less than 10 characters')
-            ],
-
+                v =>
+                    (v && v.length <= 10) ||
+                    'Password must be less than 10 characters'
+            ]
         };
     },
 
-methods: {
-    validate (){
-        if (this.$refs.form.validate()) {
-            console.debug
-            this.register
+    methods: {
+        validate() {
+            if (this.$refs.form.validate()) {
+                console.debug;
+                this.register;
+            }
         }
     }
-},
 
- //   register: (){
- //   let userData = {
- //       userName: this.username,
- //       password: this.password,
- //   }
-//},
+    //   register: (){
+    //   let userData = {
+    //       userName: this.username,
+    //       password: this.password,
+    //   }
+    //},
 
     //selectionRules: {
     //   value => !!value || 'Field is required',
     //}
-}
-
+};
 </script>
 
 <style scoped>
+.h1 {
+    padding-bottom: 20px;
+}
 
-    .h1 {
-        padding-bottom: 20px;
-    }
+.welcome {
+    position: center;
+    padding-left: 30px;
+}
 
-    .welcome {
-        position: center;
-        padding-left: 30px;
-    }
+.next {
+    padding-top: 20px;
+    padding-bottom: 100px;
+}
 
-    .next {
-        padding-top: 20px;
-        padding-bottom: 100px;
-    }
-
-    .footer {
+.footer {
     position: fixed;
     bottom: 0;
     width: 100%;
 }
 
-    .check {
+.check {
     padding-left: 22px;
     padding-bottom: 10px;
 }
 
-    .btn {
+.btn {
     width: 53%;
 }
-
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,11 +2,8 @@
     <div class="Login">
         <v-content>
             <v-container class="fill-height" fluid>
-
                 <v-row align="center" justify="center">
-
                     <v-col cols="12" sm="8" md="4">
-
                         <v-flex mb-4>
                             <div class="h1 headline text-center">
                                 <h1>Log in</h1>
@@ -14,121 +11,119 @@
                         </v-flex>
 
                         <v-card class="elevation-12">
-                            <v-toolbar color="#546e7a" light >
+                            <v-toolbar color="#546e7a" light>
                                 <div class="welcome">
-                                <v-toolbar-title>Willkommen bei sQRcrow</v-toolbar-title>
+                                    <v-toolbar-title
+                                        >Willkommen bei sQRcrow</v-toolbar-title
+                                    >
                                 </div>
                                 <v-spacer />
-
                             </v-toolbar>
                             <v-card-text>
-                                <v-form ref="form"
-                                        v-model="valid">
-
+                                <v-form ref="form" v-model="valid">
                                     <!-- <v-text-field label="name"  -->
 
                                     <v-text-field
-                                            label="Benutzername"
-                                            name="login"
-                                            :rules="nameRules"
-                                            prepend-icon="mdi-account-circle"
-                                            type="text"
+                                        label="Benutzername"
+                                        name="login"
+                                        :rules="nameRules"
+                                        prepend-icon="mdi-account-circle"
+                                        type="text"
                                     />
                                     <!--Hide or show password-->
                                     <v-text-field
-                                            id="password"
-                                            label="Passwort"
-                                            name="password"
-                                            :value="myPass"
-                                            :rules="passwordRules"
-                                            prepend-icon="mdi-lock"
-                                            :append-icon="value ? 'mdi-eye-off' : 'mdi-eye'"
-                                            @click:append="() => (value = !value)"
-                                            :type="value ? 'password' : 'text'"
-                                            :counter="10"
+                                        id="password"
+                                        label="Passwort"
+                                        name="password"
+                                        :value="myPass"
+                                        :rules="passwordRules"
+                                        prepend-icon="mdi-lock"
+                                        :append-icon="
+                                            value ? 'mdi-eye-off' : 'mdi-eye'
+                                        "
+                                        @click:append="() => (value = !value)"
+                                        :type="value ? 'password' : 'text'"
+                                        :counter="10"
                                     />
                                 </v-form>
                             </v-card-text>
 
                             <v-card-actions>
-                                <v-spacer/>
+                                <v-spacer />
 
                                 <div class="btn">
-                                    <v-btn color="#546e7a" icon=""
-                                           @click="register">Login</v-btn>
+                                    <v-btn
+                                        color="#546e7a"
+                                        icon=""
+                                        @click="register"
+                                        >Login</v-btn
+                                    >
                                 </div>
-
                             </v-card-actions>
                         </v-card>
 
                         <div class="next text-center">
-                          <p>Noch keinen Account? Melde dich <a href="/Join">hier</a> kostenlos an</p>
+                            <p>
+                                Noch keinen Account? Melde dich
+                                <a href="/Join">hier</a> kostenlos an
+                            </p>
                         </div>
-
                     </v-col>
 
                     <div class="footer">
                         <v-footer padless>
                             <v-col class="text-center" cols="12">
-                                {{ new Date().getFullYear() }} — <strong>©sQRcrow</strong>
+                                {{ new Date().getFullYear() }} —
+                                <strong>©sQRcrow</strong>
                             </v-col>
                         </v-footer>
                     </div>
-
                 </v-row>
             </v-container>
         </v-content>
-
-</div>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: 'Login',
-        props: {
-            source: String,
-        },
+export default {
+    name: 'Login',
+    props: {
+        source: String
+    },
 
-        //oder data() => ({ statt return
-        data() {
-            return {
-                value: String,
-                valid: true,
-                nameRules: [
-                    v => !!v || 'Name is requiered',
-                ],
+    //oder data() => ({ statt return
+    data() {
+        return {
+            value: String,
+            valid: true,
+            nameRules: [v => !!v || 'Name is requiered'],
 
-                passwordRules: [
-                    v => !!v || 'Password is requiered',
-                ],
-            };
-        },
+            passwordRules: [v => !!v || 'Password is requiered']
+        };
     }
+};
 </script>
 
 <style scoped>
+.h1 {
+    padding-bottom: 50px;
+}
 
-    .h1 {
-        padding-bottom: 50px;
-    }
-
-    .next {
+.next {
     padding-top: 20px;
 }
 
-    .footer {
+.footer {
     position: fixed;
     bottom: 0;
     width: 100%;
 }
 
-    .welcome {
-
+.welcome {
     padding-left: 70px;
 }
 
-    .btn {
-        width: 53%;
-    }
-
+.btn {
+    width: 53%;
+}
 </style>
