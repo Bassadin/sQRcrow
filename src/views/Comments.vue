79286b3165ccc98fd6221656579e0731a5fd1965
KarenJosten <karen.josten@hs-furtwangen.de>
2019-12-16 13:06:24 +0100

    Update Comments.vue

--- src/views/Comments.vue
+++ src/views/Comments.vue
@@ -1,22 +1,70 @@
 <template>
 <v-container>
-    <div class="comment-box" id="comment" >
+    <v-row justify="center">
+        <v-col cols="12" sm="8" md="6">
+            <v-toolbar color="navy" dark>
+                <v-toolbar-title>Kommentare</v-toolbar-title>
 
-        <v-card text-center method="post" @submit.prevent="submit">
-            <textarea v-model="data.message" class="message" name="message" id="message"  placeholder="Dein Kommentar..." required></textarea><br>
-            <!--<input v-model="data.name" class="input-name" type="text" name="name" placeholder="" required>-->
+            </v-toolbar>
+            <v-card>
+            <v-list two-line>
+                <template v-for="(item, index) in items">
+                    <v-subheader
+                            v-if="item.header"
+                            :key="item.header"
+                            inset
+                    >
+                        {{ item.header }}
+                    </v-subheader>
 
-            <v-card-actions>
+                    <v-divider
+                            v-else-if="item.divider"
+                            :key="index"
+                            inset
+                    ></v-divider>
 
-                <div class="btn">
-                    <v-btn :disabled="loading" type="submit" value="Comment">Kommentieren</v-btn>
+                    <v-list-item
+                            v-else
+                            :key="item.title"
+                            ripple
+
+                    >
+                        <v-list-item-avatar>
+                            <img :src="item.avatar">
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title v-html="item.title"></v-list-item-title>
+                            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+
+
+                </template>
+
+                <div class="comment-box" id="comment" >
+
+                    <v-content method="post" @submit.prevent="submit">
+                        <textarea v-model="data.message" class="message" id="message" name="message" placeholder="Dein Kommentar..." required></textarea><br>
+                        <!--<input v-model="data.name" class="input-name" type="text" name="name" placeholder="" required>-->
+
+                        <v-card-actions>
+                            <div class="btn">
+                                <v-btn :disabled="loading" type="submit" value="Comment">Kommentieren</v-btn>
+                            </div>
+                        </v-card-actions>
+
+                    </v-content>
                 </div>
 
-            </v-card-actions>
+            </v-list>
 
-        </v-card>
-    </div>
 
+
+            </v-card>
+
+
+        </v-col>
+    </v-row>
 </v-container>
 
 </template>
@@ -27,7 +75,52 @@
         data() {
             return {
                 loading: false,
-                data: {}
+                data: {},
+
+            items: [
+                {
+                    header: 'Aktuell',
+                },
+                { divider: true },
+                {
+                    avatar: 'https://picsum.photos/250/300?image=660',
+                    title: 'Neues aus Furtwangen',
+                    subtitle:
+                        "<span class='text--primary'>Julian</span> &mdash; Hallo",
+                },
+                {
+                    avatar: 'https://picsum.photos/250/300?image=821',
+                    title: 'Ersti Party <span class="grey--text text--lighten-1"></span>',
+                    subtitle:
+                        "<span class='text--primary'>Lea</span> &mdash; Ich wünschte ich könnte kommen.",
+                },
+                {
+                    avatar: 'https://picsum.photos/250/300?image=783',
+                    title: ':)',
+                    subtitle:
+                        "<span class='text--primary'>Sabrina</span> &mdash; hallo zusammen",
+                },
+                {
+                    header: 'Gestern',
+                },
+                { divider: true },
+                {
+                    avatar: 'https://picsum.photos/250/300?image=1006',
+                    title: 'Haustiere?',
+                    subtitle:
+                        "<span class='text--primary'>Mia</span> &mdash; Meine neue Katze, süß oder?",
+                },
+                {
+                    header: 'Letzte Woche',
+                },
+                { divider: true },
+                {
+                    avatar: 'https://picsum.photos/250/300?image=1008',
+                    title: 'Mensa',
+                    subtitle:
+                        "<span class='text--primary'>Nick</span> &mdash; Das gab es heute in der Mensa",
+                },
+            ]
             }
         },
 
@@ -57,21 +150,20 @@
     }
 </script>
 
-
 <style scoped>
 
     .comment-box {
-        position: center;
         padding-right: 300px;
-        padding-left: 300px;
+        position: center;
+        margin-right: 20px;
     }
 
     .message {
-        padding-right: 300px;
-        margin-left: 40px;
+        padding-right: 250px;
     }
 
     .btn {
-        padding-left: 200px;
+        padding-left: 140px;
     }
+
 </style>
