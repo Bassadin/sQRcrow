c2eccb969b46d56b08a88c45300a1fabfb1a180c
ManuHaa <56680795+ManuHaa@users.noreply.github.com>
2020-01-07 15:31:22 +0100

    Update Impressum.vue

--- src/views/Impressum.vue
+++ src/views/Impressum.vue
@@ -1,24 +1,35 @@
 <template>
-    <div class="Impressum">
-        <v-layout wrap text-center>
-            <v-flex xs12>
-                <h1>Impressum</h1>
-                <h2>Team Gamma MIB 2</h2>
-                <p>
+  
+  <v-app id="inspire">
+      <h1 class="header">Impressum</h1>
+    <v-card
+      class="mx-auto"
+      width="500"
+    >
+      <v-card-text>
+          
+        <div class="text--primary">
                     Angaben gemäß § 5 TMG:<br />
-
+                    MIB2 Team Gamma<br />
                     sQRcrow<br />
                     Robert-Gerwig-Platz 1<br />
                     78120 Furtwangen im Schwarzwald<br />
 
-                    Telefon: xxxxxxx<br />
-                    Telefax: xxxxxx<br />
-                    E-Mail: www.muster@web.de
-                </p>
-            </v-flex>
-        </v-layout>
-    </div>
+        </div>
+       
+      </v-card-text>
+       <v-img
+       src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
+       height="200">
+           
+            
+        </v-img>
+    </v-card>
+  </v-app>
+
 </template>
+
+
 
 <script>
 export default {
@@ -26,4 +37,17 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.text--primary{
+
+    text-align: center
+}
+
+.header{
+    
+    text-align: center
+}
+
+
+
+</style>
