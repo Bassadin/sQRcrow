61590af69f7685dd2097614969307a02f965eec0
Bastian Hodapp <bastianhodapp@gmail.com>
2020-01-10 00:22:01 +0100

    Added image upload component

--- src/views/Add_QRCode.vue
+++ src/views/Add_QRCode.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-stepper v-model="e13" vertical>
+    <v-stepper v-model="currentStepperStep" vertical>
         <v-stepper-step step="1" complete>
             Gebe deinem QR-Code einen Namen
         </v-stepper-step>
@@ -7,8 +7,9 @@
         <v-stepper-content step="1">
             <v-text-field placeholder="Name eingeben" outlined></v-text-field>
 
-            <v-btn color="primary" @click="e13 = 2">Weiter</v-btn>
-            <v-btn text>Beenden</v-btn>
+            <v-btn color="primary" @click="currentStepperStep = 2"
+                >Weiter</v-btn
+            >
         </v-stepper-content>
 
         <v-stepper-step step="2" complete>
@@ -16,10 +17,17 @@
         </v-stepper-step>
 
         <v-stepper-content step="2">
-            <v-content> <input type="file" /><br /> </v-content><br />
-
-            <v-btn color="primary" @click="e13 = 3">Weiter</v-btn>
-            <v-btn text>Beenden</v-btn>
+            <v-image-input
+                v-model="imageData"
+                :image-quality="0.85"
+                clearable
+                image-format="jpeg"
+            />
+            <br />
+            <v-btn color="primary" @click="currentStepperStep = 3"
+                >Weiter</v-btn
+            >
+            <v-btn text @click="currentStepperStep = 1">Zurück</v-btn>
         </v-stepper-content>
 
         <v-stepper-step step="3" complete>
@@ -27,19 +35,25 @@
         </v-stepper-step>
 
         <v-stepper-content step="3">
-            <v-btn color="primary" @click="e13 = 4">Upload</v-btn>
-            <v-btn text>Beenden</v-btn>
+            <v-btn color="primary" @click="currentStepperStep = 4"
+                >Fertig</v-btn
+            >
+            <v-btn text @click="currentStepperStep = 2">Zurück</v-btn>
         </v-stepper-content>
     </v-stepper>
 </template>
 
 <script>
+import VImageInput from 'vuetify-image-input';
+
 export default {
     name: 'Add-QRCode',
-
+    components: {
+        VImageInput
+    },
     data() {
         return {
-            e13: 1
+            currentStepperStep: 1
         };
     }
 };
